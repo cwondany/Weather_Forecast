dfd7b6fbfcda65d8e5c22994311950a2b0618b57
Chiara <c.wondany@outlook.de>
2017-10-27 14:04:17 +0200

    Weather app v.1

---
+++ controllers.js
@@ -0,0 +1,38 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city; 
+    });
+    
+    $scope.submit=function(){
+        $location.path("/forecast");
+    };
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=678c476303db9b6bbfbab253db8ce306", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((((1.8 * (degK - 273)) + 32)-32)*(5/9));
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
+    
+}]);
